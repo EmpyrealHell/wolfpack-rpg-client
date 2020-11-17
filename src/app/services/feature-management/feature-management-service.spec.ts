6b441fc35da602ce9fe4d06996eb6960106249d3
EmpyrealHell <empyrealhell@gmail.com>
2020-11-16 17:01:51 -0700

    added tests for feature management

--- src/app/services/feature-management/feature-management-service.spec.ts
+++ src/app/services/feature-management/feature-management-service.spec.ts
@@ -1,30 +1,53 @@
+import { not } from '@angular/compiler/src/output/output_ast';
 import { TestUtils } from 'src/test/test-utils';
-import { CommandService } from '../command/command-service';
+import { CommandCallback, CommandService } from '../command/command-service';
 import { WidgetItem } from '../widget/widget-item';
 import { WidgetService } from '../widget/widget.service';
 import { FeatureManagementService } from './feature-management-service';
 import * as data from './feature-management.json';
 
 describe('FeatureManagementService', () => {
-  let commandService: CommandService;
+  const defaultRole = ['console'];
+  const roleData = [
+    {
+      name: 'UIDeveloper',
+      widgets: ['.*'],
+    },
+    {
+      name: 'foobar',
+      widgets: ['foo', 'bar'],
+    },
+  ];
+  let commandService: jasmine.SpyObj<CommandService>;
   let widgetService: jasmine.SpyObj<WidgetService>;
   let service: FeatureManagementService;
+
+  let roleHandler: CommandCallback;
 
   beforeAll(async () => {
     commandService = (TestUtils.spyOnClass(
       CommandService
     ) as unknown) as jasmine.SpyObj<CommandService>;
+    commandService.subscribeToCommand.and.callFake(
+      (group, command, response, result, subscriber, callback) => {
+        roleHandler = callback;
+        return '';
+      }
+    );
     widgetService = (TestUtils.spyOnClass(
       WidgetService
     ) as unknown) as jasmine.SpyObj<WidgetService>;
     widgetService.getWidgets.and.returnValue([
       new WidgetItem(null, 'Console Widget', 'console', 'consoleicon'),
+      new WidgetItem(null, 'Foo', 'foo', 'fooicon'),
+      new WidgetItem(null, 'Bar', 'bar', 'baricon'),
+      new WidgetItem(null, 'Foobar', 'foo.bar', 'baricon'),
     ]);
     service = new FeatureManagementService(commandService, widgetService);
-    service.initialize();
   });
 
   it('should get default widget ids when no role has been assigned', () => {
+    service.initialize(defaultRole, roleData);
     const widgets = service.getWidgets();
     const defaultWidgets = data.defaultRole.widgets;
     for (const widget of widgets) {
@@ -33,5 +56,82 @@ describe('FeatureManagementService', () => {
     for (const widget of defaultWidgets) {
       expect(widgets.filter(x => x.id === widget).length).toBe(1);
     }
+  });
+
+  it('should get widgets for assigned roles', () => {
+    service.initialize(defaultRole, roleData, ['UIDeveloper']);
+    const widgets = service.getWidgets();
+    expect(widgets.length).toBe(4);
+    expect(widgets.filter(x => x.id === 'console').length).toBe(1);
+    expect(widgets.filter(x => x.id === 'foo').length).toBe(1);
+    expect(widgets.filter(x => x.id === 'bar').length).toBe(1);
+    expect(widgets.filter(x => x.id === 'foo.bar').length).toBe(1);
+  });
+
+  it('should inheret default widgets for non-default roles', () => {
+    service.initialize(defaultRole, roleData, ['foobar']);
+    const widgets = service.getWidgets();
+    expect(widgets.length).toBe(3);
+    expect(widgets.filter(x => x.id === 'console').length).toBe(1);
+    expect(widgets.filter(x => x.id === 'foo').length).toBe(1);
+    expect(widgets.filter(x => x.id === 'bar').length).toBe(1);
+  });
+
+  it('should throw an exception when default role has a non-existent widget', () => {
+    expect(
+      service.initialize.bind(service, ['invalid.widget'], roleData)
+    ).toThrow();
+  });
+
+  it('should throw an exception when a non-default role has a non-existent widget', () => {
+    expect(
+      service.initialize.bind(service, defaultRole, [
+        { name: 'invalid-role', widgets: ['invalid.widget'] },
+      ])
+    ).toThrow();
+  });
+
+  it('should send checkaccess command and register a responder', () => {
+    service.initialize();
+    expect(commandService.subscribeToCommand).toHaveBeenCalled();
+    const args = commandService.subscribeToCommand.calls.mostRecent().args;
+    expect(args[0]).toBe('info');
+    expect(args[1]).toBe('checkAccess');
+    expect(args[2]).toBe('responses');
+    expect(args[3]).toBe('success');
+    expect(args[4]).toBe('feature-management');
+    expect(roleHandler).not.toBeUndefined();
+    expect(commandService.sendCommand).toHaveBeenCalledWith(
+      'info',
+      'checkAccess'
+    );
+  });
+
+  it('should update role in response to checkaccess command', () => {
+    service.initialize(defaultRole, roleData);
+    roleHandler('', '', new Map<string, string>(), [
+      new Map<string, string>([['role', 'foobar']]),
+    ]);
+    const widgets = service.getWidgets();
+    expect(widgets.length).toBe(3);
+    expect(widgets.filter(x => x.id === 'console').length).toBe(1);
+    expect(widgets.filter(x => x.id === 'foo').length).toBe(1);
+    expect(widgets.filter(x => x.id === 'bar').length).toBe(1);
+  });
+
+  it('should call callback when roles are updated', () => {
+    service.initialize(defaultRole, roleData);
+    let afterUpdateWidgets: WidgetItem[] = [];
+    const updateCallback = (newWidgets: WidgetItem[]) => {
+      afterUpdateWidgets = [...newWidgets];
+    };
+    const initialWidgets = service.getWidgets(updateCallback);
+    roleHandler('', '', new Map<string, string>(), [
+      new Map<string, string>([['role', 'foobar']]),
+    ]);
+    expect(afterUpdateWidgets.length).toBe(3);
+    expect(afterUpdateWidgets.filter(x => x.id === 'console').length).toBe(1);
+    expect(afterUpdateWidgets.filter(x => x.id === 'foo').length).toBe(1);
+    expect(afterUpdateWidgets.filter(x => x.id === 'bar').length).toBe(1);
   });
 });
--- src/app/services/feature-management/feature-management-service.ts
+++ src/app/services/feature-management/feature-management-service.ts
@@ -2,9 +2,14 @@ import { Injectable } from '@angular/core';
 import { CommandService } from '../command/command-service';
 import { WidgetItem } from '../widget/widget-item';
 import { WidgetService } from '../widget/widget.service';
-import * as data from './feature-management.json';
+import * as RoleData from './feature-management.json';
 
 export type UpdateCallback = (newWidgets: WidgetItem[]) => void;
+
+export interface RoleWidgets {
+  name: string;
+  widgets: RegExp[];
+}
 
 /**
  * Service containing the feature management system.
@@ -13,16 +18,16 @@ export type UpdateCallback = (newWidgets: WidgetItem[]) => void;
   providedIn: 'root',
 })
 export class FeatureManagementService {
-  private roles: string[] = [];
+  private userRoles: string[] = [];
   private widgets: WidgetItem[] = [];
   private callback: UpdateCallback | undefined;
 
-  private defaultWidgets: string[] = [];
-  private roleWidgets: Map<string, string[]> = new Map<string, string[]>();
+  private defaultWidgets: RegExp[] = [];
+  private roleData: RoleWidgets[] = [];
 
   private verifyConfig(): void {
     const invalidWidgets = this.defaultWidgets.filter(
-      x => this.widgets.filter(y => y.id.match(x)).length === 0
+      x => this.widgets.filter(y => y.id.match(x) !== null).length === 0
     );
     if (invalidWidgets.length > 0) {
       throw (
@@ -31,11 +36,10 @@ export class FeatureManagementService {
         invalidWidgets.join(', ')
       );
     }
-    const roles = Array.from(this.roleWidgets.entries());
-    const invalidRoles = roles.filter(
+    const invalidRoles = this.roleData.filter(
       roleEntry =>
-        roleEntry[1].filter(
-          x => this.widgets.filter(y => y.id.match(x)).length === 0
+        roleEntry.widgets.filter(
+          x => this.widgets.filter(y => y.id.match(x) !== null).length === 0
         ).length > 0
     );
     if (invalidRoles.length > 0) {
@@ -45,8 +49,11 @@ export class FeatureManagementService {
         invalidRoles
           .map(
             x =>
-              `${x[0]} (${x[1]
-                .filter(y => this.widgets.filter(z => z.id.match(y)))
+              `${x.name} (${x.widgets
+                .filter(
+                  y =>
+                    this.widgets.filter(z => z.id.match(y) !== null).length > 0
+                )
                 .join(', ')})`
           )
           .join('\n')
@@ -54,14 +61,23 @@ export class FeatureManagementService {
     }
   }
 
-  private getWidgetsForRole(role: string): string[] {
-    const roleData = this.roleWidgets.get(role);
-    if (!roleData) {
-      return [];
-    }
+  private getWidgetsForDefault(): string[] {
+    const roleData = this.defaultWidgets;
     return this.widgets
       .map(x => x.id)
-      .filter(x => roleData.filter(y => x.match(y)));
+      .filter(x => roleData.filter(y => x.match(y) !== null).length > 0);
+  }
+
+  private getWidgetsForRole(role: string): string[] {
+    const roleData = this.roleData.filter(x => x.name === role);
+    if (roleData && roleData.length === 1) {
+      return this.widgets
+        .map(x => x.id)
+        .filter(
+          x => roleData[0].widgets.filter(y => x.match(y) !== null).length > 0
+        );
+    }
+    return [];
   }
 
   private handleRoleUpdate(
@@ -70,11 +86,11 @@ export class FeatureManagementService {
     group: Map<string, string>,
     subGroups: Array<Map<string, string>>
   ): void {
-    this.roles.length = 0;
+    this.userRoles.length = 0;
     for (const match of subGroups) {
       const role = match.get('role');
       if (role) {
-        this.roles.push(role);
+        this.userRoles.push(role);
       }
     }
     if (this.callback) {
@@ -87,10 +103,25 @@ export class FeatureManagementService {
     private widgetService: WidgetService
   ) {}
 
-  initialize(): void {
-    this.defaultWidgets = [...data.defaultRole.widgets];
-    data.roles.forEach(x => this.roleWidgets.set(x.name, x.widgets));
-    this.widgets.push(...this.widgetService.getWidgets());
+  initialize(
+    defaultWidgets = RoleData.defaultRole.widgets,
+    roleData = RoleData.roles,
+    userRoles: string[] = []
+  ): void {
+    this.defaultWidgets = [];
+    for (const widget of defaultWidgets) {
+      this.defaultWidgets.push(new RegExp(`^${widget}$`));
+    }
+    this.roleData = [];
+    for (const roleDatum of roleData) {
+      const roleData = { name: roleDatum.name, widgets: [] } as RoleWidgets;
+      this.roleData.push(roleData);
+      for (const widget of roleDatum.widgets) {
+        roleData.widgets.push(new RegExp(`^${widget}$`));
+      }
+    }
+    this.userRoles = [...userRoles];
+    this.widgets = [...this.widgetService.getWidgets()];
     this.verifyConfig();
     this.commandService.subscribeToCommand(
       'info',
@@ -110,9 +141,9 @@ export class FeatureManagementService {
     if (updateCallback) {
       this.callback = updateCallback;
     }
-    const widgets = [...this.defaultWidgets];
-    if (this.roles.length > 0) {
-      for (const role of this.roles) {
+    const widgets = [...this.getWidgetsForDefault()];
+    if (this.userRoles.length > 0) {
+      for (const role of this.userRoles) {
         widgets.push(...this.getWidgetsForRole(role));
       }
     }
